6a5c29e2a201143ade905c93ec0506240195482a
Gustavo Silva Candido Cabral <gustavo.cabral@rarolabs.com.br>
2021-12-13 20:32:43 -0300

    create posts entities in domain

---
+++ lib/app/features/posts/domain/entities/posts_entities.dart
@@ -0,0 +1,12 @@
+class PostsEntities {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+  PostsEntities({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.body,
+  });
+}
